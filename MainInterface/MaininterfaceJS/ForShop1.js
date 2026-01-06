03b9fe0bb3c6012e707fa92d6cb8496021fe712d
kevincalendas <johnkevixcalendas@gmail.com>
2026-01-06 23:02:32 +0800

    added features: moreoptions

--- MainInterface/MaininterfaceJS/ForShop1.js
+++ MainInterface/MaininterfaceJS/ForShop1.js
@@ -1,5 +1,63 @@
 // For Opening the main product overview //
 
+function OpenCloseMoreOptions() {
+    const menu = document.getElementById('MoreOptionMenuWindow');
+    const button1 = document.getElementById('OptionMenu1');
+    const button2 = document.getElementById('OptionMenu2');
+    const button3 = document.getElementById('OptionMenu3');
+    const button4 = document.getElementById('OptionMenu4');
+    
+    const currentDisplay = window.getComputedStyle(menu).display;
+
+    if (currentDisplay === "none") {
+        menu.style.display = "grid"; 
+        setTimeout(() => {
+            menu.style.opacity = "1";
+            menu.classList.remove('is-close');
+            menu.classList.add('is-open');
+            setTimeout(() => {
+                button1.style.scale = "1";
+                button1.style.opacity = "1";
+                button1.style.transition = "all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);";
+                setTimeout(() => {
+                    button2.style.scale = "1";
+                    button2.style.opacity = "1";
+                    button2.style.transition = "all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+                    setTimeout(() => {
+                        button3.style.scale = "1";
+                        button3.style.opacity = "1";
+                        button3.style.transition = "all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+                        setTimeout(() => {
+                            button4.style.scale = "1";
+                            button4.style.opacity = "1";
+                            button4.style.transition = "all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)";
+                        }, 50);
+                    }, 50);
+                }, 50);
+            }, 250);
+        }, 1);
+    } else {
+        menu.classList.remove('is-open');
+        menu.classList.add('is-close');
+        button1.style.opacity = "0";
+        button1.style.transition = "all 0.1s ease-out";
+        button2.style.opacity = "0";
+        button2.style.transition = "all 0.1s ease-out";
+        button3.style.opacity = "0";
+        button3.style.transition = "all 0.1s ease-out";
+        button4.style.opacity = "0";
+        button4.style.transition = "all 0.1s ease-out";
+        button1.style.scale = "0.5";
+        button2.style.scale = "0.5";
+        button3.style.scale = "0.5";
+        button4.style.scale = "0.5";
+        setTimeout(() => {
+            menu.style.opacity = "0";
+            menu.style.display = "none";
+        }, 400);
+    }
+}
+
 function OpenMainShopWindow() {
     const MainBackgroundBlurShop = document.getElementById('MainBackgroundBlurShop');
     const ProductMainWindow = document.getElementById('ProductMainWindow');
